3294c4bca2865a25876d5266f4276efd575fc1a6
timothy mitchell <timothy.p.mitchell92@gmail.com>
2017-04-10 16:46:03 -0700

    have first store done

--- salesApp.js
+++ salesApp.js
@@ -8,19 +8,35 @@ var firstandPike = {
   }
 
 function getRandomInt(min,max) {
-  min= Math.ceil(23);
-  max = Math.floor(65);
+  min = Math.ceil(min);
+  max = Math.floor(max);
   return Math.floor(Math.random() * (max-min)) + min;
 }
+var hours = ['6am:','7am:','8am:','9am:','10am:','11am:','12am:',
+'1am:','2am:','3am:','4am:','5am:','6am:','7am:','8am:']
 
-for (var i = 0; i<14; i++) {
+for (var i = 0; i < 15; i++) {
   var avgFirstandPike = getRandomInt(firstandPike.min,firstandPike.max);
-  console.log('Random number of customers,', avgFirstandPike)
+  // console.log('Random number of customers,', avgFirstandPike);
   var pikeCookies = avgFirstandPike * firstandPike.avgCookies;
-  console.log(i + 6,'am, ', 'Avg cookies per hour', pikeCookies)
-  firstandPike.numOfCookies.push(pikeCookies)
+  // Math.ceil(pikeCookies)
+  // console.log(i + 6,'am, ', 'Avg cookies per hour', pikeCookies);
+  firstandPike.numOfCookies.push(pikeCookies);
+}
+var totalcookies = 0;
+for (var i = 0; i< firstandPike.numOfCookies.length;i++){
+  totalcookies = totalcookies + firstandPike.numOfCookies[i];
 }
 
+var cookiesUl = document.getElementById('cookies');
+var cookiesLi;
+for (var i = 0; i < firstandPike.numOfCookies.length; i++){
+  cookiesLi = document.createElement('li');
+  cookiesLi.setAttribute('class','firstandPike.numOfCookies');
+  cookiesLi.textContent = hours[i] + ' ' + firstandPike.numOfCookies[i];
+  cookiesUl.appendChild(cookiesLi);
+}
+cookiesLi.textContent = 'Total number of cookies: ' + totalcookies
 
 
 
