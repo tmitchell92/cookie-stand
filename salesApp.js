10baf3bfc8619c62b7ecdb201a6705304763c2c5
timothy mitchell <timothy.p.mitchell92@gmail.com>
2017-04-10 15:18:36 -0700

    did first store

--- salesApp.js
+++ salesApp.js
@@ -3,13 +3,63 @@
 var firstandPike = {
   min:[23],
   max:[65],
-  avg_cookies:[6.3],
+  avgCookies:[6.3],
+  numOfCookies:[]
   }
 
 function getRandomInt(min,max) {
-  min= Math.ceil(1);
-  max = Math.floor(100);
-  return Math.floor(Math.random() * max-min) + min;
+  min= Math.ceil(23);
+  max = Math.floor(65);
+  return Math.floor(Math.random() * (max-min)) + min;
 }
 
-var avg_cookies = (getRandomInt(0,100))
+for (var i = 0; i<14; i++) {
+  var avgFirstandPike = getRandomInt(firstandPike.min,firstandPike.max);
+  console.log('Random number of customers,', avgFirstandPike)
+  var pikeCookies = avgFirstandPike * firstandPike.avgCookies;
+  console.log(i + 6,'am, ', 'Avg cookies per hour', pikeCookies)
+  firstandPike.numOfCookies.push(pikeCookies)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var seaTacAirport = {
+  min:[3],
+  max:[24],
+  avgCookies:[1.2],
+}
+
+var seattleCenter = {
+  min:[11],
+  max:[38],
+  avgCookies:[3.7],
+}
+
+var capitolHill = {
+  min:[20],
+  max:[38],
+  avgCookies:[2.3],
+}
+
+var alki = {
+  min:[2],
+  max:[16],
+  avgCookies:[4.6],
+}
